70558549c8915705a852736c9b398abbacd168f4
sono <abdelrhmanstorm123@gmail.com>
2024-12-14 19:12:26 +0200

    finished

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: 'http://alasser0s.github.io/restaurant-app/', // Replace with your GitHub repository name
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
-
-export default nextConfig;
+module.exports = nextConfig;

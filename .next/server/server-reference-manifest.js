a2b72ed9e183c3259e3d0a45dcd55288caecac1d
sono <abdelrhmanstorm123@gmail.com>
2024-12-15 07:06:06 +0200

    i do not know anymore

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"rYKZht06/LnH8U67JlJDjx9zpGPmGUTnocOT3AEpoXc=\"}"
